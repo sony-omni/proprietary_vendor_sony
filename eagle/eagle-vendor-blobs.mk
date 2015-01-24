d3a55f13326658e40c7cab15025b289914693a56
Erik Castricum <erikcas1972@gmail.com>
2015-01-24 21:43:15 +0100

    Revert "add prebuilt libcameraservice.so"
    This reverts commit f28a9ca25e2f12bf816c0fe978418411d686ddd0.
    
    Change-Id: I008ffdc4fec7b87d1da50768bf1dbe3c8e4c4980

--- eagle/eagle-vendor-blobs.mk
+++ eagle/eagle-vendor-blobs.mk
@@ -87,7 +87,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/eagle/proprietary/lib/libyasalgo.so:system/lib/libyasalgo.so \
     vendor/sony/eagle/proprietary/vendor/firmware/libpn547_fw.so:system/vendor/firmware/libpn547_fw.so \
     vendor/sony/eagle/proprietary/vendor/lib/libbt-vendor.so:system/vendor/lib/libbt-vendor.so \
-    vendor/sony/eagle/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
     vendor/sony/eagle/proprietary/vendor/lib/lib-imscamera.so:system/vendor/lib/lib-imscamera.so \
     vendor/sony/eagle/proprietary/vendor/lib/libchromatix_SKUAA_ST_gc0339_common.so:system/vendor/lib/libchromatix_SKUAA_ST_gc0339_common.so \
     vendor/sony/eagle/proprietary/vendor/lib/libchromatix_SKUAA_ST_gc0339_default_video.so:system/vendor/lib/libchromatix_SKUAA_ST_gc0339_default_video.so \
